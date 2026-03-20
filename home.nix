844be32f700befb07035fe9f4472205ca9d91c63
Yesterday17 <mmf@mmf.moe>
2026-03-21 01:59:44 +0900

    feat: home settings

--- home.nix
+++ home.nix
@@ -5,4 +5,43 @@
   home.stateVersion = "25.11";
 
   home.packages = [ pkgs.claude-code ];
+  home.shellAliases.lcc = "claude";
+
+  # Git
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Yesterday17";
+      user.email = "mmf@mmf.moe";
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      rerere.enabled = true;
+    };
+  };
+
+  # Zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history = {
+      size = 50000;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+    };
+  };
+
+  # Direnv - auto-load .envrc
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  # GPG
+  programs.gpg.enable = true;
+  home.file.".gnupg/gpg-agent.conf".text = ''
+    pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
+  '';
 }
